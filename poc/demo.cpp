5462eb4d26e7fb6847a4cdab640d9c459bf7b2b6
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-11-16 14:33:05 +0100

    updated proof of concept

--- poc/demo.cpp
+++ poc/demo.cpp
@@ -1,4 +1,6 @@
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <iostream>
 
@@ -12,8 +14,19 @@ int main() {
     std::cout << "creating socket for webserver..." << std::endl;
     int serverFd = socket(domain, type, protocol);
 
+    // define specific ip and port
+    struct sockaddr_in intServerSockAddr;
+    intServerSockAddr.sin_family = AF_INET;
+    intServerSockAddr.sin_addr.s_addr = inet_addr("0.0.0.0");
+    intServerSockAddr.sin_port = htons(8080);
+
+    // bind the socket to the ip and port
+    bind(serverFd, (sockaddr*)&intServerSockAddr, sizeof(intServerSockAddr));
+
+    // todo: Listening and queueing etc..
+
+
 
     // closing socket for webserver
     close(serverFd);
-
 }
