dfe1736d70a37f3a62ed9b80579645f931bc3066
itopchu <mr.ibrahim.topchu@outlook.com>
2024-03-09 02:16:40 +0100

    Validation puase for little update

--- inc/http/RequestValidate.hpp
+++ inc/http/RequestValidate.hpp
@@ -18,69 +18,58 @@
 # include <iostream>
 # include <fstream>
 // filesystem management
-// # include <filesystem>
 # include "Exceptions.hpp"
 
 class RequestValidate
 {
 	private:
-		HTTPmethod					requestMethod;
-		t_path						requestPath;
+		ConfigServer const*			_requestConfig;
+		HTTPmethod					_requestMethod;
+		t_path						_requestPath;
 
-		t_path						execPath;
+		t_path						_execPath;
 		bool						_autoIndex;
 		bool						_isCGI;
 		bool						_isFile;
-		
 
-		Location*					validLocation;
-		Parameters*					validParams;
-		std::string					targetDir;
-		std::string					targetFile;
+		Location*					_validLocation;
+		Parameters*					_validParams;
+		t_path						targetDir;
+		t_path						targetFile;
 		std::vector<std::string>	folders;
 
-		ConfigServer const*			config;
-		size_t						statusCode;
-		t_path						execDir;
+		size_t						_statusCode;
 
-		Location*			_getValidLocation() const;
-		Parameters*			_getValidParams() const;
-		const std::string&	_getTargetDir() const;
-		const std::string&	_getTargetFile() const;
-
-		void		_setTargetFile(const std::string& file);
-		void		_setValidParams(const Parameters* params);
-		void		_setTargetDir(const std::string& dir);
 		void		_setStatusCode(const size_t& code);
-		void		_setAutoIndex(bool index);
 
 		void		_separateFolders(std::string const& input, std::vector<std::string>& output);
 		Location*	_diveLocation(Location& cur, std::vector<std::string>::iterator itDirectory);
 
-		void		_initValidLocation(void);
-		void		_initTargetDir(void);
+		void		_initValidLocation( void );
+		void		_initTargetDir( void );
 
-		bool		_handleFolder(void);
-		bool		_handleFile(void);
-		void		_handleStatus(void);
-		bool		_handleReturns(void);
-		bool		_handleErrorCode(void);
-		bool		_handleServerPages(void);
+		bool		_handleFolder( void );
+		bool		_handleFile( void );
+		void		_handleStatus( void );
+		bool		_handleReturns( void );
+		bool		_handleErrorCode( void );
+		bool		_handleServerPages( void );
 
 	public:
-		RequestValidate(void);
+		RequestValidate( void );
+		virtual	~RequestValidate( void );
 
 		void	setConfig( ConfigServer const* );
 		void	setMethod( HTTPmethod );
 		void	setPath( t_path const& );
-	
+		void	solvePath( void );
+
+		Parameters*		getValidParams( void ) const;
 		t_path const&	getExecPath( void ) const;
 		int				getStatusCode( void ) const;
-		bool			isCGI() const;
-		bool			isAutoIndex() const;
-		bool			isFile() const;
-		void			solvePath( void );
-		virtual 		~RequestValidate(void) {};
+		bool			isAutoIndex( void ) const;
+		bool			isFile( void ) const;
+		bool			isCGI( void ) const;
 };
 
 #endif
