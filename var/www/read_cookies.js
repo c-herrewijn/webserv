e988ae90b3c9b81a55d9e95e8985e75776e622d0
Christiaan Herrewijn <cherrewi@student.codam.nl>
2024-03-29 19:21:21 +0100

    read user name from cookies on homepage

---
+++ var/www/read_cookies.js
@@ -0,0 +1,23 @@
+function display_name_from_cookies() {
+    const cookieStr = document.cookie;
+    let key, value
+    let first_name = "";
+    let last_name = "";
+    console.log("hello world");
+
+    const cookieArr = cookieStr.split('; ');
+    for (let i = 0; i < cookieArr.length; i++) {
+        [key, value] = cookieArr[i].split('=');
+        if (key == "first_name") {
+            first_name = value
+        }
+        if (key == "last_name") {
+            last_name = value
+        }
+    }
+
+    const full_name = (first_name || last_name)
+        ? (`${first_name} ${last_name}`).trim()
+        : 'anonymous user';
+    document.getElementById("welcome-name").innerHTML = full_name;
+}
